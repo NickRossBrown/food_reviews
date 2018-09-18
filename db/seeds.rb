a8e035d5aa5eb03e1b94a4e76186a1e2553275c9
Nick Brown <nickrossbrown@gmail.com>
2018-09-17 17:23:10 -0700

    seed a random number of reviews for each food item

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,13 @@
 
 require 'faker'
 
-@user = User.create( email: "NICKexample@gmail.com", password: "eeeeee")
+@user = User.create( email: "example@gmail.com", password: "eeeeee")
 50.times do |i|
   @food = @user.foods.build(:name => Faker::Food.fruits, :price => 1+rand(4), :location=> Faker::Address.country)
   @food.save!
+  @random_number = 1+rand(4)
+  @random_number.times do
+    @review = @food.reviews.new(:description => Faker::BojackHorseman.quote, :user_rating => 1+rand(4))
+    @review.save!
+  end
 end
